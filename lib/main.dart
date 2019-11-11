e88f76865a52973755f06dc64e698731d89e9e8b
Daniel Stahl <danstahl1138@gmail.com>
2019-11-11 15:11:03 -0600

    added storage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:demo_api_app_flutter/storage/api_key.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -53,6 +53,7 @@ const List<String> choices = const <String>[
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
   String _selectedModel=choices[0];
+  Future<List<ApiKey>> _key=retrieveKey();
 
   void _incrementCounter() {
     setState(() {
@@ -68,9 +69,10 @@ class _MyHomePageState extends State<MyHomePage> {
     setState((){
       _selectedModel=choice;
     });
-    // Causes the app to rebuild with the new _selectedChoice.
     
   }
+
+
   
   @override
   Widget build(BuildContext context) {
@@ -121,7 +123,9 @@ class _MyHomePageState extends State<MyHomePage> {
                           }),
                       new FlatButton(
                           child: const Text('OPEN'),
-                          onPressed: () {
+                          onPressed: () async {
+                            await insertKey(ApiKey(id:1, key:"need to add key here"));
+                            _key=retrieveKey();
                             Navigator.pop(context);
                           })
                     ],
@@ -153,36 +157,47 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
         body: TabBarView(
           children:[
-            Center(
-              // Center is a layout widget. It takes a single child and positions it
-              // in the middle of the parent.
-              child: Column(
-                // Column is also a layout widget. It takes a list of children and
-                // arranges them vertically. By default, it sizes itself to fit its
-                // children horizontally, and tries to be as tall as its parent.
-                //
-                // Invoke "debug painting" (press "p" in the console, choose the
-                // "Toggle Debug Paint" action from the Flutter Inspector in Android
-                // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-                // to see the wireframe for each widget.
-                //
-                // Column has various properties to control how it sizes itself and
-                // how it positions its children. Here we use mainAxisAlignment to
-                // center the children vertically; the main axis here is the vertical
-                // axis because Columns are vertical (the cross axis would be
-                // horizontal).
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    'You have pushed the button this many times:',
-                  ),
-                  Text(
-                    '$_counter',
-                    style: Theme.of(context).textTheme.display1,
-                  ),
-                ],
-              ),
+            FutureBuilder<List<ApiKey>>(
+              builder: (context, snapshot){
+                if(snapshot.hasData && snapshot.data.length>0){
+                  return Center(
+                    // Center is a layout widget. It takes a single child and positions it
+                    // in the middle of the parent.
+                    child: Column(
+                      // Column is also a layout widget. It takes a list of children and
+                      // arranges them vertically. By default, it sizes itself to fit its
+                      // children horizontally, and tries to be as tall as its parent.
+                      //
+                      // Invoke "debug painting" (press "p" in the console, choose the
+                      // "Toggle Debug Paint" action from the Flutter Inspector in Android
+                      // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+                      // to see the wireframe for each widget.
+                      //
+                      // Column has various properties to control how it sizes itself and
+                      // how it positions its children. Here we use mainAxisAlignment to
+                      // center the children vertically; the main axis here is the vertical
+                      // axis because Columns are vertical (the cross axis would be
+                      // horizontal).
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(
+                          'You have pushed the button this many times:',
+                        ),
+                        Text(
+                          '$_counter',
+                          style: Theme.of(context).textTheme.display1,
+                        ),
+                      ],
+                    ),
+                  );
+                }
+                else{
+                  return Text("You need to add your API key");
+                }
+              },
+              future: _key
             ),
+            
             Icon(Icons.directions_transit),
             Icon(Icons.directions_bike),
           ]
---
+++ lib/storage/api_key.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+
+import 'package:path/path.dart';
+import 'package:sqflite/sqflite.dart';
+class ApiKey {
+  final int id;
+  final String key;
+  ApiKey({this.id, this.key});
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'key': key
+    };
+  }
+}
+
+//var dbPath = await getDatabasesPath();
+final Future<Database> database = openDatabase(
+  // Set the path to the database.
+  'api_key_store.db',
+  // When the database is first created, create a table to store the api keys.
+  onCreate: (db, version) {
+    // Run the CREATE TABLE statement on the database.
+    return db.execute(
+      "CREATE TABLE api_key_store(id INTEGER PRIMARY KEY, key VARCHAR(64) NOT NULL)",
+    );
+  },
+  // Set the version. This executes the onCreate function and provides a
+  // path to perform database upgrades and downgrades.
+  version: 1,
+);
+
+Future<void> insertKey(ApiKey key) async {
+  // Get a reference to the database.
+  final Database db = await database;
+
+  // Insert the key into the correct table. You might also specify the
+  // `conflictAlgorithm` to use in case the same api key is inserted twice.
+  //
+  // In this case, replace any previous data.
+  await db.insert(
+    'api_key_store',
+    key.toMap(),
+    conflictAlgorithm: ConflictAlgorithm.replace,
+  );
+}
+
+Future<List<ApiKey>>retrieveKey() async {
+  // Get a reference to the database.
+  final Database db = await database;
+
+  // Query the table for all The Dogs.
+  final List<Map<String, dynamic>> maps = await db.query('api_key_store');
+
+  // Convert the List<Map<String, dynamic> into a List<Dog>.
+  return List.generate(maps.length, (i) {
+    return ApiKey(
+      id: maps[i]['id'],
+      key: maps[i]['key'],
+    );
+  });
+}
