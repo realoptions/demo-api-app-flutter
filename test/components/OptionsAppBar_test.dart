f20b8af8761700bb91deaa20224f426ee7b8cdf4
Daniel Stahl <danstahl1138@gmail.com>
2019-12-11 13:21:20 -0600

    added tests for api key as well

--- test/components/OptionsAppBar_test.dart
+++ test/components/OptionsAppBar_test.dart
@@ -25,6 +25,10 @@ void main() {
     when(db.retrieveKey()).thenAnswer((_) => Future.value([key]));
   }
 
+  void stubInsertKey() {
+    when(db.insertKey(any)).thenAnswer((_) => Future.value());
+  }
+
   testWidgets('AppBar displays with no inputs', (WidgetTester tester) async {
     stubRetrieveData();
     await tester.pumpWidget(MaterialApp(
@@ -60,5 +64,53 @@ void main() {
     await tester.pumpAndSettle();
     expect(find.text("SomeTitle: CGMY"), findsOneWidget);
     expect(find.text("CGMY"), findsNothing);
+  });
+  testWidgets('AppBar api key works', (WidgetTester tester) async {
+    stubRetrieveData();
+    await tester.pumpWidget(MaterialApp(
+        home: Material(
+            child: BlocProvider<ApiBloc>(
+                bloc: ApiBloc(db: db),
+                child: BlocProvider<SelectModelBloc>(
+                    bloc: SelectModelBloc(),
+                    child: Scaffold(
+                        appBar: OptionsAppBar(
+                            choices: modelChoices, title: "SomeTitle")))))));
+    await tester.pumpAndSettle();
+    expect(find.text("SomeTitle: Heston"), findsOneWidget);
+    await tester.tap(find.byIcon(Icons.lock));
+    await tester.pumpAndSettle();
+    expect(find.text("somekey"), findsOneWidget); //apikey
+    expect(find.text("CANCEL"), findsOneWidget);
+    await tester.tap(find.text("CANCEL"));
+    await tester.pumpAndSettle();
+    expect(find.text("CANCEL"), findsNothing);
+  });
+  testWidgets('AppBar api key works when changing',
+      (WidgetTester tester) async {
+    stubRetrieveData();
+    stubInsertKey();
+    await tester.pumpWidget(MaterialApp(
+        home: Material(
+            child: BlocProvider<ApiBloc>(
+                bloc: ApiBloc(db: db),
+                child: BlocProvider<SelectModelBloc>(
+                    bloc: SelectModelBloc(),
+                    child: Scaffold(
+                        appBar: OptionsAppBar(
+                            choices: modelChoices, title: "SomeTitle")))))));
+    await tester.pumpAndSettle();
+    expect(find.text("SomeTitle: Heston"), findsOneWidget);
+    await tester.tap(find.byIcon(Icons.lock));
+    await tester.pumpAndSettle();
+    expect(find.text("somekey"), findsOneWidget); //apikey
+    expect(find.text("CANCEL"), findsOneWidget);
+    await tester.enterText(find.byType(TextFormField), "new api key");
+    await tester.tap(find.text("SAVE"));
+    await tester.pumpAndSettle();
+    expect(find.text("CANCEL"), findsNothing);
+    await tester.tap(find.byIcon(Icons.lock));
+    await tester.pumpAndSettle();
+    expect(find.text("new api key"), findsOneWidget); //new apikey
   });
 }
