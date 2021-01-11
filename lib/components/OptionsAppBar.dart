ae828bdb43ee8ce0c95f9ae4e400a0ce228616d1
Daniel STahl <danstahl1138@gmail.com>
2021-01-10 20:49:56 -0600

    adjusted options

--- lib/components/OptionsAppBar.dart
+++ lib/components/OptionsAppBar.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:realoptions/blocs/select_model/select_model_bloc.dart';
-import 'package:realoptions/blocs/bloc_provider.dart';
 import 'package:realoptions/models/models.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -68,62 +67,6 @@ class OptionsAppBar extends StatelessWidget with PreferredSizeWidget {
         ],
       );
     });
-    /*return StreamBuilder<Model>(
-        stream: selectBloc.outSelectedModel,
-        initialData: this.choices[0],
-        builder: (buildContext, snapshot) {
-          var selectedModel = snapshot.data;
-          return AppBar(
-            key: Key("AppBarComponent"),
-            title: Text(this.title + ": " + selectedModel.label),
-            actions: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.help),
-                  onPressed: () {
-                    showDialog(
-                        context: context,
-                        builder: (BuildContext context) {
-                          return AlertDialog(
-                              title: Text('Help'),
-                              content: SingleChildScrollView(
-                                child: ListBody(
-                                  children: <Widget>[
-                                    Text(
-                                        '''This app calculates option prices for three models: Heston, CGMY with a diffusion and a stochastic clock, and Merton jump-diffusion with stochastic clock.  It uses Fang and Oosterlee's algorithm for efficient pricing across many strikes.'''),
-                                    RaisedButton(
-                                      onPressed: _launchDocs,
-                                      child: Text('References'),
-                                    ),
-                                  ],
-                                ),
-                              ));
-                        });
-                  }),
-              IconButton(
-                icon: Icon(Icons.more_vert),
-                onPressed: () {
-                  showModalBottomSheet<void>(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return ListView(
-                            shrinkWrap: true,
-                            children: this
-                                .choices
-                                .map((choice) => RadioListTile<Model>(
-                                    title: Text(choice.label),
-                                    value: choice,
-                                    groupValue: selectedModel,
-                                    onChanged: (choice) {
-                                      selectBloc.setModel(choice);
-                                      Navigator.pop(context);
-                                    }))
-                                .toList());
-                      });
-                },
-              ),
-            ],
-          );
-        });*/
   }
 }
 
--- lib/pages/options.dart
+++ lib/pages/options.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:realoptions/blocs/options/options_bloc.dart';
+import 'package:realoptions/blocs/options/options_state.dart';
 import 'package:realoptions/models/progress.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
@@ -5,8 +8,6 @@ import 'package:realoptions/models/response.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:realoptions/utils/chart_utils.dart' as utils;
 import 'package:realoptions/components/CustomPadding.dart' as padding;
-import 'package:realoptions/blocs/bloc_provider.dart';
-import 'file:///home/daniel/Documents/code/finside/demo-api-app-flutter/test/components/options_bloc.dart';
 
 class ShowOptionPrices extends StatelessWidget {
   const ShowOptionPrices({
@@ -14,97 +15,84 @@ class ShowOptionPrices extends StatelessWidget {
   });
   @override
   Widget build(BuildContext context) {
-    final OptionsBloc bloc = BlocProvider.of<OptionsBloc>(context);
-    return StreamBuilder<StreamProgress>(
-        stream: bloc.outOptionsProgress,
-        initialData: StreamProgress.Busy,
-        builder: (buildContext, snapshot) {
-          switch (snapshot.data) {
-            case StreamProgress.Busy:
-              return Center(child: CircularProgressIndicator());
-            case StreamProgress.NoData:
-              return Center(child: Text("Please submit parameters!"));
-            case StreamProgress.DataRetrieved:
-              return _OptionPrices();
-            default:
-              return Center(child: CircularProgressIndicator());
-          }
-        });
+    return BlocBuilder<OptionsBloc, OptionsState>(builder: (context, data) {
+      if (data is NoData) {
+        return Center(child: Text("Please submit parameters!"));
+      } else if (data is IsOptionsFetching) {
+        return Center(child: CircularProgressIndicator());
+      } else if (data is OptionsData) {
+        return _OptionPrices(options: data.options);
+      } else {
+        return Center(child: CircularProgressIndicator());
+      }
+    });
   }
 }
 
 class _OptionPrices extends StatelessWidget {
+  final Map<String, List<ModelResult>> options;
+  _OptionPrices({@required this.options});
   @override
   Widget build(BuildContext context) {
-    final OptionsBloc bloc = BlocProvider.of<OptionsBloc>(context);
     final ThemeData themeData = Theme.of(context);
     final charts.Color callColor = utils.convertColor(themeData.primaryColor);
     final charts.Color putColor = utils.convertColor(themeData.accentColor);
-    return StreamBuilder<Map<String, List<ModelResult>>>(
-        stream: bloc.outOptionsController,
-        builder: (buildContext, snapshot) {
-          if (snapshot.hasError) {
-            return Center(child: Text(snapshot.error.toString()));
-          }
-          if (snapshot.data == null) {
-            return Center(child: CircularProgressIndicator());
-          }
-          var callPrices = snapshot.data["call"];
-          var putPrices = snapshot.data["put"];
-          var optionSeries = [
-            charts.Series(
-              id: 'Call Prices',
-              domainFn: (ModelResult optionData, _) => optionData.atPoint,
-              measureFn: (ModelResult optionData, _) => optionData.value,
-              colorFn: (ModelResult optionData, _) => callColor,
-              data: callPrices,
-            ),
-            charts.Series(
-              id: 'Put Prices',
-              domainFn: (ModelResult optionData, _) => optionData.atPoint,
-              measureFn: (ModelResult optionData, _) => optionData.value,
-              colorFn: (ModelResult optionData, _) => putColor,
-              data: putPrices,
-            )
-          ];
-          var domain = utils.getDomain(callPrices);
 
-          var optionChart = charts.LineChart(
-            optionSeries,
-            animate: true,
-            domainAxis: charts.NumericAxisSpec(tickProviderSpec: domain),
-            behaviors: [charts.SeriesLegend()],
-          );
-          var ivSeries = [
-            charts.Series(
-              id: 'Implied Volatility',
-              data: callPrices,
-              domainFn: (ModelResult optionData, _) => optionData.atPoint,
-              measureFn: (ModelResult optionData, _) => optionData.iv,
-              colorFn: (ModelResult optionData, _) => putColor,
-            )
-          ];
-          var range = utils.getIVRange(callPrices);
-          var ivChart = charts.LineChart(
-            ivSeries,
-            animate: true,
-            domainAxis: charts.NumericAxisSpec(tickProviderSpec: domain),
-            primaryMeasureAxis: charts.NumericAxisSpec(tickProviderSpec: range),
-            behaviors: [charts.SeriesLegend()],
-          );
-          var children = [
-            padding.PaddingForm(
-              child: optionChart,
-            ),
-            padding.PaddingForm(
-              child: ivChart,
-            ),
-          ];
-          return OrientationBuilder(builder: (context, orientation) {
-            return GridView.count(
-                crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
-                children: children);
-          });
-        });
+    final callPrices = options["call"];
+    final putPrices = options["put"];
+    var optionSeries = [
+      charts.Series(
+        id: 'Call Prices',
+        domainFn: (ModelResult optionData, _) => optionData.atPoint,
+        measureFn: (ModelResult optionData, _) => optionData.value,
+        colorFn: (ModelResult optionData, _) => callColor,
+        data: callPrices,
+      ),
+      charts.Series(
+        id: 'Put Prices',
+        domainFn: (ModelResult optionData, _) => optionData.atPoint,
+        measureFn: (ModelResult optionData, _) => optionData.value,
+        colorFn: (ModelResult optionData, _) => putColor,
+        data: putPrices,
+      )
+    ];
+    var domain = utils.getDomain(callPrices);
+
+    var optionChart = charts.LineChart(
+      optionSeries,
+      animate: true,
+      domainAxis: charts.NumericAxisSpec(tickProviderSpec: domain),
+      behaviors: [charts.SeriesLegend()],
+    );
+    var ivSeries = [
+      charts.Series(
+        id: 'Implied Volatility',
+        data: callPrices,
+        domainFn: (ModelResult optionData, _) => optionData.atPoint,
+        measureFn: (ModelResult optionData, _) => optionData.iv,
+        colorFn: (ModelResult optionData, _) => putColor,
+      )
+    ];
+    var range = utils.getIVRange(callPrices);
+    var ivChart = charts.LineChart(
+      ivSeries,
+      animate: true,
+      domainAxis: charts.NumericAxisSpec(tickProviderSpec: domain),
+      primaryMeasureAxis: charts.NumericAxisSpec(tickProviderSpec: range),
+      behaviors: [charts.SeriesLegend()],
+    );
+    var children = [
+      padding.PaddingForm(
+        child: optionChart,
+      ),
+      padding.PaddingForm(
+        child: ivChart,
+      ),
+    ];
+    return OrientationBuilder(builder: (context, orientation) {
+      return GridView.count(
+          crossAxisCount: orientation == Orientation.portrait ? 1 : 2,
+          children: children);
+    });
   }
 }
